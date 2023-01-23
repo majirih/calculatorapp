3df79ee559f79fd4dcee154bbdde82547e426842
majirih <86539930+majirih@users.noreply.github.com>
2023-01-23 14:57:54 +0100

    defined variables and queried them inorder to fetch data from docment

---
+++ index.js
@@ -0,0 +1,6 @@
+const numberButton = document.querySelectorAll('[data-number]');
+const operationButton = document.querySelectorAll('[data-operation]');
+const equalToButton = document.querySelectorAll('[data-equal-to]');
+const allClearButton = document.querySelectorAll('[data-all-clear]');
+const previousCalcTextElement = document.querySelectorAll('[data-output-previous-calc]');
+const presentCalcTextElement = document.quuerySelectorALL('[data-output-present-calc]');
