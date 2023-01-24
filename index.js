e721f17287411f13bd878480cf2ac283b57687f4
majirih <86539930+majirih@users.noreply.github.com>
2023-01-24 06:58:59 +0100

    applied JS functions to the calc

--- index.js
+++ index.js
@@ -1,69 +1,113 @@
-class Calculator {
+class Calculator {  
   constructor(previousCalcTextElement, presentCalcTextElement) {
     this.previousCalcTextElement = previousCalcTextElement;
     this.presentCalcTextElement = presentCalcTextElement;
     this.clear();
   }
+
+  clear() {
+    this.presentCalc = "";
+    this.previousCalc = "";
+    this.operation = undefined;
+  }
+
+  deleteInput() {
+    this.presentCalc = this.presentCalc.toString().slice(0, -1);
+  }
+
+  appendNumber(number) {
+    if (number === "." && this.presentCalc.includes(".")) return;
+    this.presentCalc = this.presentCalc.toString() + number.toString();
+  }
+
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousCalc);
+    const present = parseFoat(this.presentCalc);
+    if (isNaN(prev) || isNaN(present)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + present;
+        break;
+      case "-":
+        computation = prev - present;
+        break;
+      case "*":
+        computation = prev * present;
+        break;
+      case "÷":
+        computation = prev / present;
+        break;
+      default:
+        return;
+    }
+    this.presentCalc = computation;
+    this.operation = undefined;
+    this.previousCalc = "";
+  }
+
+  selectOperation(operation) {
+    if (this.presentCalc === "") return;
+    if (this.previousCalc !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousCalc = this.presentCalc;
+    this.presentCalc = "";
+  }
+
+  updateDisplay() {
+    this.presentCalcTextElement.innerText = this.presentCalc;
+    if (this.operation != null) {
+      this.previousCalcTextElement.innerText = `${this.previousCalc} ${this.operation}`;
+    } else {
+      this.previousCalcTextElement.innerText = "";
+    }
+  }
 }
 
-const numberButton = document.querySelectorAll("[data-number]");
-const operationButton = document.querySelectorAll("[data-operation]");
-const equalToButton = document.querySelector("[data-equal-to]");
-const allClearButton = document.querySelector("[data-all-clear]");
+const numberButton = document.querySelectorAll("#data-number");
+const operationButton = document.querySelectorAll("#data-operation");
+const equalToButton = document.querySelector("#data-equal-to");
+const deleteButton = document.querySelector("#data-delete");
+const allClearButton = document.querySelector("#data-all-clear");
 const previousCalcTextElement = document.querySelector(
-  "[data-output-previous-calc]"
+  "#data-output-previous-calc"
 );
-const presentCalcTextElement = document.quuerySelector(
-  "[data-output-present-calc]"
+const presentCalcTextElement = document.querySelector(
+  "#data-output-present-calc"
 );
 
-clear();
-{
-  this.presentCalc = "";
-  this.previousCalc = "";
-  this.operation = undefined;
-}
 const calculator = new Calculator(
   previousCalcTextElement,
   presentCalcTextElement
 );
 
-numberButton.foreach((button) => {
+numberButton.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.appendNumber(button.innerText);
-    calculator.updateDisplay(
-      (this.presentCalcTextElement.innerText = this.prsentCalc)
-    );
+    calculator.updateDisplay();
   });
 });
 
-appendNumber(number);
-{
-  if (number === "." && this.presentCalc.includes(".")) return;
-  this.presentCalc = this.presentCalc.toString() + number.toString();
-}
-
-operationButton.foreach((button) => {
+operationButton.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.selectOperation(button.innerText);
     calculator.updateDisplay();
   });
 });
 
-selectOperation(operation);
-{
-  if (this.presentCalc === "") return;
-  if (this.previousCalc !== "") {
-    this.compute();
-  }
-  this.operation = operation;
-  this.previousCalc = this.presentCalc;
-  this.presentCalc = "";
-}
-
 equalToButton.addEventListener("click", (button) => {
   calculator.compute();
   calculator.updateDisplay();
 });
 
-compute()
+allClearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", (button) => {
+  calculator.deleteInput();
+  calculator.updateDisplay();
+});
