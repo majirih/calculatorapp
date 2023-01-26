709d50b7ec9c1ddbc11377b5ab5016a801acae1a
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-01-26 11:10:17 +0100

    bug fix

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
-class Calculator {  
-  constructor(previousCalcTextElement, presentCalcTextElement) {
-    this.previousCalcTextElement = previousCalcTextElement;
-    this.presentCalcTextElement = presentCalcTextElement;
-    this.clear();
-  }
+class Calculator {
+	constructor(previousCalcTextElement, presentCalcTextElement) {
+		this.previousCalcTextElement = previousCalcTextElement;
+		this.presentCalcTextElement = presentCalcTextElement;
+		this.clear();
+	}
 
-  clear() {
-    this.presentCalc = "";
-    this.previousCalc = "";
-    this.operation = undefined;
-  }
+	clear() {
+		this.presentCalc = "";
+		this.previousCalc = "";
+		this.operation = undefined;
+	}
 
-  deleteInput() {
-    this.presentCalc = this.presentCalc.toString().slice(0, -1);
-  }
+	deleteInput() {
+		this.presentCalc = this.presentCalc.toString().slice(0, -1);
+	}
 
-  appendNumber(number) {
-    if (number === "." && this.presentCalc.includes(".")) return;
-    this.presentCalc = this.presentCalc.toString() + number.toString();
-  }
+	appendNumber(number) {
+		if (number === "." && this.presentCalc.includes(".")) return;
+		this.presentCalc = this.presentCalc.toString() + number.toString();
+	}
 
-  compute() {
-    let computation;
-    const prev = parseFloat(this.previousCalc);
-    const present = parseFoat(this.presentCalc);
-    if (isNaN(prev) || isNaN(present)) return;
-    switch (this.operation) {
-      case "+":
-        computation = prev + present;
-        break;
-      case "-":
-        computation = prev - present;
-        break;
-      case "*":
-        computation = prev * present;
-        break;
-      case "÷":
-        computation = prev / present;
-        break;
-      default:
-        return;
-    }
-    this.presentCalc = computation;
-    this.operation = undefined;
-    this.previousCalc = "";
-  }
+	compute() {
+		let computation;
+		const prev = parseFloat(this.previousCalc);
+		const present = parseFloat(this.presentCalc);
+		if (isNaN(prev) || isNaN(present)) return;
+		switch (this.operation) {
+			case "+":
+				computation = prev + present;
+				break;
+			case "-":
+				computation = prev - present;
+				break;
+			case "*":
+				computation = prev * present;
+				break;
+			case "÷":
+				computation = prev / present;
+				break;
+			default:
+				return;
+		}
+		this.presentCalc = computation;
+		this.operation = undefined;
+		this.previousCalc = "";
+	}
 
-  selectOperation(operation) {
-    if (this.presentCalc === "") return;
-    if (this.previousCalc !== "") {
-      this.compute();
-    }
-    this.operation = operation;
-    this.previousCalc = this.presentCalc;
-    this.presentCalc = "";
-  }
+	selectOperation(operation) {
+		if (this.presentCalc === "") return;
+		if (this.previousCalc !== "") {
+			this.compute();
+		}
+		this.operation = operation;
+		this.previousCalc = this.presentCalc;
+		this.presentCalc = "";
+	}
 
-  updateDisplay() {
-    this.presentCalcTextElement.innerText = this.presentCalc;
-    if (this.operation != null) {
-      this.previousCalcTextElement.innerText = `${this.previousCalc} ${this.operation}`;
-    } else {
-      this.previousCalcTextElement.innerText = "";
-    }
-  }
+	updateDisplay() {
+		this.presentCalcTextElement.innerText = this.presentCalc;
+		if (this.operation != null) {
+			this.previousCalcTextElement.innerText = `${this.previousCalc} ${this.operation}`;
+		} else {
+			this.previousCalcTextElement.innerText = "";
+		}
+	}
 }
 
 const numberButton = document.querySelectorAll("#data-number");
@@ -72,42 +72,44 @@ const equalToButton = document.querySelector("#data-equal-to");
 const deleteButton = document.querySelector("#data-delete");
 const allClearButton = document.querySelector("#data-all-clear");
 const previousCalcTextElement = document.querySelector(
-  "#data-output-previous-calc"
+	"#data-output-previouscalc"
 );
 const presentCalcTextElement = document.querySelector(
-  "#data-output-present-calc"
+	"#data-output-presentcalc"
 );
 
+console.log(previousCalcTextElement, presentCalcTextElement);
+
 const calculator = new Calculator(
-  previousCalcTextElement,
-  presentCalcTextElement
+	previousCalcTextElement,
+	presentCalcTextElement
 );
 
 numberButton.forEach((button) => {
-  button.addEventListener("click", () => {
-    calculator.appendNumber(button.innerText);
-    calculator.updateDisplay();
-  });
+	button.addEventListener("click", () => {
+		calculator.appendNumber(button.innerText);
+		calculator.updateDisplay();
+	});
 });
 
 operationButton.forEach((button) => {
-  button.addEventListener("click", () => {
-    calculator.selectOperation(button.innerText);
-    calculator.updateDisplay();
-  });
+	button.addEventListener("click", () => {
+		calculator.selectOperation(button.innerText);
+		calculator.updateDisplay();
+	});
 });
 
 equalToButton.addEventListener("click", (button) => {
-  calculator.compute();
-  calculator.updateDisplay();
+	calculator.compute();
+	calculator.updateDisplay();
 });
 
 allClearButton.addEventListener("click", (button) => {
-  calculator.clear();
-  calculator.updateDisplay();
+	calculator.clear();
+	calculator.updateDisplay();
 });
 
 deleteButton.addEventListener("click", (button) => {
-  calculator.deleteInput();
-  calculator.updateDisplay();
+	calculator.deleteInput();
+	calculator.updateDisplay();
 });
